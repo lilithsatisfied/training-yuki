c58b174cf762b14e00857bf23af62940ad8f52d8
Yuki Tsuji <lilith.satisfied@gmail.com>
2025-03-31 14:59:30 +0900

    Add User model and migration

--- api_sample/db/schema.rb
+++ api_sample/db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_250_330_234_320) do
-  create_table 'books', force: :cascade do |t|
-    t.string 'title'
-    t.string 'author'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+ActiveRecord::Schema[7.1].define(version: 2025_03_31_055528) do
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end

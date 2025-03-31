79dfb701b762a44d04b9723ce79a68a108855281
Yuki Tsuji <lilith.satisfied@gmail.com>
2025-03-31 13:54:34 +0900

    Run initial auto-correct

--- api_sample/spec/requests/api/v1/books_spec.rb
+++ api_sample/spec/requests/api/v1/books_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Books", type: :request do
-  describe "GET /index" do
-    it "returns all books" do
+RSpec.describe 'Api::V1::Books', type: :request do
+  describe 'GET /index' do
+    it 'returns all books' do
       create_list(:book, 3)
-      get "/api/v1/books", headers: { Accept: "application/json" }
+      get '/api/v1/books', headers: { Accept: 'application/json' }
       expect(response).to have_http_status(:ok)
       json = JSON.parse(response.body)
       expect(json.length).to eq(3)

8d474a2fd1dfce589651843f077518eaf4fbd8da
Yuki Tsuji <lilith.satisfied@gmail.com>
2025-03-31 13:43:59 +0900

    Add RSpec tests for Book model and API

---
+++ api_sample/spec/requests/api/v1/books_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::Books", type: :request do
+  describe "GET /index" do
+    it "returns all books" do
+      create_list(:book, 3)
+      get "/api/v1/books", headers: { Accept: "application/json" }
+      expect(response).to have_http_status(:ok)
+      json = JSON.parse(response.body)
+      expect(json.length).to eq(3)
+    end
+  end
+end
